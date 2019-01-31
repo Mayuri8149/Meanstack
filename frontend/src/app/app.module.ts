31faf15a67a8c365ddf5b1a0181ed6f2544f3c4a
Mayuri8149 <kate.mayuri14792@gmail.com>
2019-01-31 10:35:17 +0530

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//import { AppRoutingModule, Routes } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule , MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule,MatButtonModule, MatCardModule, MatTableModule, MatDividerModule, MatSnackBarModule} from '@angular/material';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+
+
+import { IssueService } from './issue.service';
+const routes : Routes = [
+
+  { path: 'create', component: CreateComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full'}
+
+];
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    //AppRoutingModule.forRoot(routes),
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule,MatButtonModule, MatCardModule, MatTableModule, MatDividerModule, MatSnackBarModule,
+    HttpClientModule
+  ],
+  providers: [IssueService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
